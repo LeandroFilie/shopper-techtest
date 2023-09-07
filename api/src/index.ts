fc6d08f36123233a7c837d81776ba5f60dbad986
LeandroFilie <leandro.gfilie@gmail.com>
2023-09-07 17:12:28 -0300

    config: settings api server

---
+++ api/src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { AddressInfo } from 'net';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const server = app.listen(process.env.PORT || 3003, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server running at http://localhost:${address.port}`);
+  } else {
+    console.error('Failure initializing server.');
+  }
+});
